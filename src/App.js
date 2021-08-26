fbbe57c8ecf3244827c9c9b7a37b849cb8d9071c
Caio Francisco <caio.francisco@quintoandar.com.br>
2021-08-26 13:50:28 -0300

    using hooks

--- src/App.js
+++ src/App.js
@@ -1,17 +1,26 @@
+import React, { useState } from 'react';
+import Button from './components/Button/Button';
 import SnackBar from './components/SnackBar/SnackBar';
 import './App.css';
 
 function App() {
-
+  const [snackBarState, setSnackBarState] = useState(false);
+  function openSnackBar() {
+    setSnackBarState(true);
+  }
   return (
     <div className="App">
+      <Button
+            onClick={openSnackBar}
+        >Executar ação</Button>
       <SnackBar
         text="Ação concluída com êxito. Tente novamente maiss tarde"
-        openButtonLabel="Executar ação"
         snackBarButtonLabel="Tente novamente"
         type="success"
         id="my-snack-bar"
         time={5000}
+        open={snackBarState}
+        onClose={() => setSnackBarState(false)}
       />
     </div>
   );
--- src/components/SnackBar/SnackBar.js
+++ src/components/SnackBar/SnackBar.js
@@ -1,8 +1,8 @@
+import { useEffect, useState } from 'react';
 import './SnackBar.css';
-import Button from '../Button/Button';
 
-const SnackBar = ({ text, snackBarButtonLabel, openButtonLabel, type, id, buttonFunction, time }) => {
-    const timeoutList = [];
+const SnackBar = ({ text, snackBarButtonLabel, type, id, time, open, onClose }) => {
+    const timeout = [];
     const types = {
         error: {
             icon: (() => (<svg className="MuiSvgIcon-root jss6" style={{color: types.error.color}} focusable="false" viewBox="0 0 24 24" color="inherit" aria-hidden="true" fill="none" aria-label="[object Object]"><path fill="currentColor" d="M12 13.2l4.05 4.05 1.2-1.2L13.2 12l4.05-4.05-1.2-1.2L12 10.8 7.95 6.75l-1.2 1.2L10.8 12l-4.05 4.05 1.2 1.2L12 13.2z"></path><path fill="currentColor" fill-rule="evenodd" d="M1.5 12C1.5 6.15 6.15 1.5 12 1.5S22.5 6.15 22.5 12 17.85 22.5 12 22.5 1.5 17.85 1.5 12zM3 12c0 4.95 4.05 9 9 9s9-4.05 9-9-4.05-9-9-9-9 4.05-9 9z" clip-rule="evenodd"></path></svg>)),
@@ -18,43 +18,35 @@ const SnackBar = ({ text, snackBarButtonLabel, openButtonLabel, type, id, button
         }
     }
 
-    function toggleSnackBar() {
-        let elem = document.getElementById('my-snack-bar');
-        let colorBar = document.getElementById('color-bar');
-        elem.classList.add('visible');
-        colorBar.style.animation = `close-color-bar ${time/1000}s .5s`;
-        timeoutList.push(setTimeout(() => {
-            elem.classList.remove('visible')
-        }, time));
-        elem.classList.add('close');
-        timeoutList.push(setTimeout(() => {
-            elem.classList.remove('close'); 
-            colorBar.style.animation = "";
-        }, time + 500));
-    }
+    const getSnackBarClassName = () => (open ? "snack-bar visible" : "snack-bar");
 
-    function closeSnackBar() {
-        let elem = document.getElementById('my-snack-bar');
-        let colorBar = document.getElementById('color-bar');
-        elem.classList.remove('visible');
-        elem.classList.add('close');
+    useEffect(() => {
+        setSnackBarClassName(getSnackBarClassName());
+        if (open) {
+            console.log("inserting timeout");
+            setTimeout(() => {
+                closeSnackBar();
+            }, time);
+        }
+    }, [open]);
+
+    const [snackBarClassName, setSnackBarClassName] = useState(getSnackBarClassName());
+    
+    const closeSnackBar = () => {
+        console.log(timeout);
+        setSnackBarClassName("snack-bar close");
+        console.log("setSnackBarClassName('snack-bar close')");
         setTimeout(() => {
-            elem.classList.remove('close'); 
-            colorBar.style.animation = "";
-            while (timeoutList.length) {
-                clearTimeout(timeoutList.pop());
-            }
-        }, 500); 
+            setSnackBarClassName("snack-bar");
+            onClose();
+        }, 500);
     }
-
 
     return (
         <>
-        <Button
-            onClick={toggleSnackBar}
-        >{openButtonLabel}</Button>
-        <div className="snack-bar" id={id} >
-            <div className="color-bar" id="color-bar" style={{backgroundColor: types[type].color}}></div>
+        
+        <div className={snackBarClassName} id={id} >
+            <div className={"color-bar"} id="color-bar" style={{backgroundColor: types[type].color, animation: open ? `close-color-bar ${time/1000}s .5s` : ""}}></div>
             <div className="container">
                 <div className="content">
                     <div className="icon">
